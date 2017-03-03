635bbcbb27d641e44044284c0bbab0555b69c0eb
Alexandra Tooley <sandratooley@rocketmail.com>
2017-03-02 20:27:40 -0700

    Q1project

--- KitchFix/index.js
+++ KitchFix/index.js
@@ -1,56 +1,79 @@
 $().ready(() => {
 
-   $('ul.tabs').tabs('select_tab', 'tab_id');
+    $('ul.tabs').tabs('select_tab', 'tab_id');
+    $('#submit-ask').click(function(event) {
+        event.preventDefault();
+    let question = $('#question').val();
+    $.ajax({
+        url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/quickAnswer?q=${question}%3F`,
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
 
-  $('#joke').click(function(event) {
-      event.preventDefault();
-  // $.get("https://api.whatdoestrumpthink.com/api/v1/quotes/personalized?q=yourname", function(data, status){
-  //   return data;
-  //   console.log(data)
-  // });
-  $.ajax({
-      url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/jokes/random`,
-      type: 'GET',
-      dataType: 'json',
-      success: function(data) {
+            console.log(data)
+            let answers = data.answer
+            $('#answer').append(answers)
+        },
 
-          console.log(data)
-          let joke = data.text
-          $('#joking').append(joke)
-          },
+        error: function(err) {
+            console.log(err);
+        },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "0PAFuTUgNnmshTiXtoGA99RngNJxp1QWghAjsnZgHGsB5IUPwr");
+        }
+    })
+  })
+    $('#joke').click(function(event) {
+        event.preventDefault();
+        // $.get("https://api.whatdoestrumpthink.com/api/v1/quotes/personalized?q=yourname", function(data, status){
+        //   return data;
+        //   console.log(data)
+        // });
+        $.ajax({
+            url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/jokes/random`,
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
 
-          error: function(err) {
-              console.log(err);
-          },
-          beforeSend: function(xhr) {
-              xhr.setRequestHeader("X-Mashape-Authorization", "0PAFuTUgNnmshTiXtoGA99RngNJxp1QWghAjsnZgHGsB5IUPwr");
-          }})
+                console.log(data)
+                let joke = data.text
+                $('#joking').append(joke)
+            },
+
+            error: function(err) {
+                console.log(err);
+            },
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader("X-Mashape-Authorization", "0PAFuTUgNnmshTiXtoGA99RngNJxp1QWghAjsnZgHGsB5IUPwr");
+            }
         })
-        console.log(joke);
-  $('#trivia').click(function(event) {
-      event.preventDefault();
-      $.ajax({
-          url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/trivia/random`,
-          type: 'GET',
-          dataType: 'json',
-          success: function(data) {
+    })
+    console.log(joke);
+    $('#trivia').click(function(event) {
+        event.preventDefault();
+        $.ajax({
+            url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/trivia/random`,
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
 
-              console.log(data)
-              let trivia = data.text
-              $('#info').append(trivia)
-              },
+                console.log(data)
+                let trivia = data.text
+                $('#info').append(trivia)
+            },
 
-              error: function(err) {
-                  console.log(err);
-              },
-              beforeSend: function(xhr) {
-                  xhr.setRequestHeader("X-Mashape-Authorization", "0PAFuTUgNnmshTiXtoGA99RngNJxp1QWghAjsnZgHGsB5IUPwr");
-              }})
-            })
+            error: function(err) {
+                console.log(err);
+            },
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader("X-Mashape-Authorization", "0PAFuTUgNnmshTiXtoGA99RngNJxp1QWghAjsnZgHGsB5IUPwr");
+            }
+        })
+    })
     console.log('we did it');
     var $recipes = $('#recipes');
     $('#submit-menu').click(function(event) {
-      console.log("submitted form and feeling pretty good about that");
+        console.log("submitted form and feeling pretty good about that");
 
         event.preventDefault();
         let diet = $('#diet').val();
@@ -67,58 +90,63 @@ $().ready(() => {
                 console.log('success', data);
                 // let ds = data.Search
                 let newObject = {}
-                for (i =0; i < data.meals.length; i++) {
-                   newObject[data.meals[i].id] = {
-                     title: data.meals[i].title
+                for (i = 0; i < data.meals.length; i++) {
+                    newObject[data.meals[i].id] = {
+                        title: data.meals[i].title
 
-                   }
+                    }
                 }
                 console.log(newObject);
                 for (keys in newObject)
-                $.ajax({
-                    url:`https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${keys}/information`,
-                  type: 'GET',
-                  dataType: 'json',
-                  success: function(data){
-                    // newObject[keys].image= mealPhoto
-                  console.log(data)
-                  // for (object in data) {
-                  let image = data.image
-                  let title = data.title
-                  let minutes = data.preparationMinutes
-                  let instructions = data.instructions
-                  console.log(title);
-                  let col = $('<div>').addClass('col s6 m6 l4')
-                  let card = $('<div>').addClass('card hoverable')
-                  let cardTitle = $('<h6>').addClass('card-title center').text(data.title)
-                  let content = $('<div>').addClass('card-content clicky center')
-                  let link = $('<a>').attr('href', '#modal2')
-                  let recPic = $('<img>').attr({src: data.image, id: data.id, data: data.title})
-                  $('#recipes').append(col)
-                  col.append(card)
-                  card.append(content)
-                  content.append(link)
-                  link.append(recPic)
-                  content.append(cardTitle)
+                    $.ajax({
+                        url: `https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/${keys}/information`,
+                        type: 'GET',
+                        dataType: 'json',
+                        success: function(data) {
+                            // newObject[keys].image= mealPhoto
+                            console.log(data)
+                            // for (object in data) {
+                            let image = data.image
+                            let title = data.title
+                            let minutes = data.preparationMinutes
+                            let instructions = data.instructions
+                            console.log(title);
+                            let col = $('<div>').addClass('col s6 m6 l4')
+                            let card = $('<div>').addClass('card hoverable')
+                            let cardTitle = $('<h6>').addClass('card-title center').text(data.title)
+                            let content = $('<div>').addClass('card-content clicky center')
+                            let link = $('<a>').attr('href', '#modal2')
+                            let recPic = $('<img>').attr({
+                                src: data.image,
+                                id: data.id,
+                                data: data.title
+                            })
+                            $('#recipes').append(col)
+                            col.append(card)
+                            card.append(content)
+                            content.append(link)
+                            link.append(recPic)
+                            content.append(cardTitle)
 
-                  $('img').click(function(){
-                   let target = event.target.id
-                    let recTitle = $('<h5>').text(title).attr('style','text-align: center')
-                   let recInstructions = $('<p>').text(instructions).attr('style', 'text-align center')
+                            $('img').click(function() {
+                                let target = event.target.id
+                                let recTitle = $('<h5>').text(title).attr('style', 'text-align: center')
+                                let recInstructions = $('<p>').text(instructions).attr('style', 'text-align center')
 
-                   $('#instructions').append(recTitle)
-                           $('#instructions').append(recInstructions)
+                                $('#instructions').append(recTitle)
+                                $('#instructions').append(recInstructions)
 
-                         })
+                            })
 
-                },
-                // },
-                error: function(err) {
-                    console.log(err);
-                },
-                beforeSend: function(xhr) {
-                    xhr.setRequestHeader("X-Mashape-Authorization", "0PAFuTUgNnmshTiXtoGA99RngNJxp1QWghAjsnZgHGsB5IUPwr");
-                }})
+                        },
+                        // },
+                        error: function(err) {
+                            console.log(err);
+                        },
+                        beforeSend: function(xhr) {
+                            xhr.setRequestHeader("X-Mashape-Authorization", "0PAFuTUgNnmshTiXtoGA99RngNJxp1QWghAjsnZgHGsB5IUPwr");
+                        }
+                    })
 
 
             },
